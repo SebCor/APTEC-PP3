48501cfb5c88121707496fb4c1010072d45b0881
Sebastian Cordoba <Seb.Cor71@gmail.com>
2019-11-04 21:16:50 -0600

    INICIO DEL PP3

---
+++ APTEC/src/main/java/com/mycompany/aptec/MAIN.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.aptec;
+
+/**
+ *
+ * @author Sebastian
+ */
+public class MAIN {
+    
+     public static void main(String[] args) {
+        
+        
+        
+    }
+    
+}
